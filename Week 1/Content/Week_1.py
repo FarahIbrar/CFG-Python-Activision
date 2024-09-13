892e4914a99c2fdbee8982a5d588dcffee85b500
Farah Ibrar <farah11ibrar@gmail.com>
2024-09-13 17:59:55 +0100

    Add files via upload

---
+++ Week 1/Content/Week_1.py
@@ -0,0 +1,50 @@
+print('Hello, World!')
+
+print('My name is Farah.')
+print(2 * (10 + 3))
+
+print('CAT' * 3)
+print('CAT ' + 'Meow')
+
+# Cute Python code with adorable output
+print("💕🐱 Hello! You're purr-fect! 🐱💕")
+print("  /\\_/\\  ")
+print(" ( o.o ) ")
+print("  > ^ <  ")
+
+username = 'Farah Ibrar'
+Age = 25
+print(Age)
+
+exp = 2 ** 4
+con = str(exp)
+print(type(exp))
+print(type(con))
+
+print('I hope it is sunny this weekend')
+
+Subtract = 5-6
+print(Subtract)
+
+Multiplication = 8*9
+print(Multiplication)
+
+Divide = 6/2
+print(Divide)
+
+# Divide_2 = 5/0
+# print(Divide_2)
+# The error message we'll be seeing here, ZeroDivisionError: division by zero,
+# It occurs when you try to divide a number by zero, which is mathematically undefined.
+
+Float_division = 5.0/2
+print(Float_division)
+
+Brackets = 2 * (10 + 3)
+print(Brackets)
+
+Exponent_x_to_the_power_of_y = 2 ** 4
+print(Exponent_x_to_the_power_of_y)
+
+Modulo_remainder = 5%2
+print(Modulo_remainder)
